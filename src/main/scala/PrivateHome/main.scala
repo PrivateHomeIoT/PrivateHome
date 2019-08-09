ae158da0a09ddb57d88a0ad62e22a00cf151a59c
RaHoni <Honi2002suess@gmail.com>
2019-08-09 18:36:45 +0200

    Commandline Parsing

--- src/main/scala/PrivateHome/main.scala
+++ src/main/scala/PrivateHome/main.scala
@@ -1,18 +1,25 @@
 package PrivateHome
 
+import scala.util.Try
+
 object main {
   def main(args: Array[String]): Unit = {
     println("Hello World")
 
-    /*
+    for (x <- args) println(x)
+    println(args.length)
+
     val Mhz = new MHz_Connect()
     var Command: Boolean = true
     var x: String = "11111"
     var y: String = "10000"
 
 
-    val x:Array[Char] = Array('1','1','1','1','1')
-    val y:Array[Char] = Array('1','0','0','0','0')
+    if (args.length == 3) {
+      x = args(0)
+      y = args(1)
+      Command = Try(args(2).toBoolean).getOrElse(false)
+    }
 
 
 
