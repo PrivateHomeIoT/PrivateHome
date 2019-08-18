61caa2a23c07b6e7de4da911dc397dbacf625415
Maximilian Inckmann <maincky@gmail.com>
2019-08-18 22:43:23 +0200

    Finishing first version of adding objects to devices.xml and id.xml. This is necessery to protect the configuration and other settings of all devices in case of an uncontrolled shutdown which could have the consequence of losing these data.

--- src/main/scala/PrivateHome/editXML.scala
+++ src/main/scala/PrivateHome/editXML.scala
@@ -7,25 +7,30 @@ class editXML {
   def addObject() {
 
     var device = XML.load("src/main/scala/PrivateHome/devices.xml")
-    //var idXml = XML.load("src/main/scala/PrivateHome/id.xml")
+    var idXml = XML.load("src/main/scala/PrivateHome/id.xml")
+    println(device)
+    print(idXml)
 
     val name = "object.getName()"
     val id = "object.getId()"
     val room = "object.getRoom()"
     val typE = "object.getType()"
-    //if(object.getType == 443){
+    //if(object.getType == MHz){
     //    val systemCode = object.getSystemCode()
     //    val unitCode = object.getUnitCode()
     //    }
     val systemCode = "object.getSystemCode()"
     val unitCode = "object.getUnitCode()"
-    val test = <object id ={id}><name>+ name +</name><room>+ room +</room><type>+ typE +</type><systemCode>+ systemCode +</systemCode><unitCode>+ unitCode +</unitCode></object>
-    val result = addANode(device, test)
 
+    val id_prep = <id>{id}</id>
+    val id_result = addANode(idXml,id_prep)
+    XML.save("src/main/scala/PrivateHome/id.xml", id_result)
 
+    val prep = <object id ={id}>&#xD;&#9;<name>{name}</name>&#xD;&#9;<room>{room}</room>&#xD;&#9;<type>{typE}</type>&#xD;&#9;<systemCode>{systemCode}</systemCode>&#xD;&#9;<unitCode>{unitCode}+</unitCode>&#xD;&#9;</object>
+    val result = addANode(device, prep)
     XML.save("src/main/scala/PrivateHome/devices.xml", result)
-    // "<object id='" + id +"'><name>"+ name + "</name><room>"+room+"</room><type>"+typE+"</type><systemCode>"+systemCode+"</systemCode><unitCode>"+unitCode+"</unitCode></object></device>"
   }
+
   def addANode (to: Node, newNode: Node) = to match {
     case Elem (prefix, label, attributes, scope, child@_*) => Elem (prefix, label, attributes, scope, child ++ newNode: _*)
     case _ => println(" could not find node ");
