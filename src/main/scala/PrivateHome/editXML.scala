18df678a9d3cf4c6b541cb0c22d940c6d62eb4da
Maximilian Inckmann <maincky@gmail.com>
2019-09-21 20:08:15 +0200

    Now you can add full XML via <editXML>.addElem(XML:Elem,id:String).

--- src/main/scala/PrivateHome/editXML.scala
+++ src/main/scala/PrivateHome/editXML.scala
@@ -1,5 +1,6 @@
 package PrivateHome
 
+import scala.collection.JavaConverters._
 import scala.xml._
 
 class editXML {
@@ -28,24 +29,34 @@ class editXML {
 
     val prep =
       <object id={id}>
-        &#9; <name>
-        {name}
-      </name>
-        &#9; <room>
-        {room}
-      </room>
-        &#9; <type>
-        {typE}
-      </type>
-        &#9; <systemCode>
-        {systemCode}
-      </systemCode>
-        &#9; <unitCode>
-        {unitCode}
-      </unitCode>
+        &#9; <name>{name}</name>
+        &#9; <room>{room}</room>
+        &#9; <type>{typE}</type>
+        &#9; <systemCode>{systemCode}</systemCode>
+        &#9; <unitCode>{unitCode}</unitCode>
       </object>
     val result = addANode(device, prep)
     XML.save("src/main/scala/PrivateHome/devices.xml", result)
+  }
+
+  def addElem(input : Elem, id: String) = {
+    val device = XML.load("src/main/scala/PrivateHome/devices.xml")
+    val idXml = XML.load("src/main/scala/PrivateHome/id.xml")
+
+    val id_prep = <id>{id}</id>
+    val id_result = addANode(idXml, id_prep)
+    XML.save("src/main/scala/PrivateHome/id.xml", id_result)
+
+    val result = addANode(device, input)
+    XML.save("src/main/scala/PrivateHome/devices.xml", result)
+  }
+
+  def loadObject(id: String): collection.Seq[Node] = {
+    val bigData = XML.load("src/main/scala/PrivateHome/devices.xml")
+    val children = bigData.child.foldLeft(NodeSeq.Empty)((acc, elem) => if((elem \ "@id").text == id) acc else acc ++ elem)
+    //println(bigData \\ children)
+    val result = bigData.child
+    return result
   }
 
   def removeObject(id: String): Unit = {
@@ -57,7 +68,7 @@ class editXML {
     val result = devices.copy(child = children)
 
     val id_prep = <id>{id}</id>
-    val id_children = deleteNodes(idXml, (elem) => elem.text == id_prep.text)
+    val id_children = deleteNodes(idXml, elem => elem.text == id_prep.text)
     //val id_children: NodeSeq = idXml.child.foldLeft(NodeSeq.Empty)(op = (acc_id, elem_id) => if (acc_id.text == id.toString) acc_id else acc_id ++ elem_id)
     val id_result = idXml.copy(child = id_children)
 
@@ -71,22 +82,11 @@ class editXML {
     XML.save("src/main/scala/PrivateHome/devices.xml", result)
   }
 
-  private def deleteNodes(n: Elem, f: (Node) => Boolean): NodeSeq = n.child.foldLeft(NodeSeq.Empty)((acc, elem) => if (f(elem)) acc else acc ++ elem)
+  private def deleteNodes(n: Elem, f: Node => Boolean): NodeSeq = n.child.foldLeft(NodeSeq.Empty)((acc, elem) => if (f(elem)) acc else acc ++ elem)
 
   private def addANode(to: Node, newNode: Node): Node = to match {
     case Elem(prefix, label, attributes, scope, child@_*) => Elem(prefix, label, attributes, scope, child ++ newNode: _*)
-    case _ => println(" could not find node ");
+    case _ => println(" could not find node ")
       to
   }
-  /* def deleteNodesWithValue(n: Elem, value: String): Elem = {
-    val children = deleteNodes(n, (elem) => elem.text == value)
-    //val children = n.child.foldLeft(NodeSeq.Empty)((acc, elem) => if (acc.text == value) acc else acc ++ elem)
-    n.copy(child = children)
-  } */
-
-  /*private def deleteNodesWithAttributeValue(n: Elem, value: String) = {
-    val children = devices.child.foldLeft(NodeSeq.Empty)((acc, elem) => if((elem \ "@id").text == id) acc else acc ++ elem)
-    devices.copy(child = children)
-  }
-  */
 }
