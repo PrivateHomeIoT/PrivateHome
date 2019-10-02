c71b6490416beb1e5a9a265dac4f7eed3aa0e586
Maximilian Inckmann <maincky@gmail.com>
2019-10-03 00:29:05 +0200

    Basic methods of mqttSwitch.scala: - Now you can turn on a MQTT-Device with the id. - Now you can turn off a MQTT-Device with the id. - Now you can generate a XML-Node for an MQTT-Switch.

--- src/main/scala/PrivateHome/Devices/MQTT/mqttSwitch.scala
+++ src/main/scala/PrivateHome/Devices/MQTT/mqttSwitch.scala
@@ -1,5 +1,23 @@
 package PrivateHome.Devices.MQTT
 
-class mqttSwitch {
+import PrivateHome.Devices.Switch
+
+import scala.xml.Node
+
+case class mqttSwitch(ID: String, keepStatus: Boolean) extends Switch(ID, keepStatus) {
+
+  def on(): Unit = mqttClient.publish("Home/switch/cmnd/" + id, "ON")
+
+  def off(): Unit = mqttClient.publish("Home/switch/cmnd/" + id, "OFF")
+
+  override def toXml: Node = <switch id={ID}>
+    <type>MQTT</type>
+    <id>
+      {ID}
+    </id>
+    <keepStatus>
+      {keepStatus}
+    </keepStatus>
+  </switch>
 
 }
