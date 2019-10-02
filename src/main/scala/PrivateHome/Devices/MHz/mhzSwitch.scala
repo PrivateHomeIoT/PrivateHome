dec00fdec51978b09760f02b5fa21333925f7896
RaHoni <honi2002suess@gmail.com>
2019-10-02 23:09:58 +0200

    Beautified Code for better readability.

--- src/main/scala/PrivateHome/Devices/MHz/mhzSwitch.scala
+++ src/main/scala/PrivateHome/Devices/MHz/mhzSwitch.scala
@@ -1,16 +1,16 @@
 package PrivateHome.Devices.MHz
 
 import PrivateHome.Devices.Switch
-import PrivateHome.Devices.MHz.sendMhz
 
 import scala.xml.Node
 
 /**
  * An Switch class that uses 433MHz to communicate to the Switch
- * @param setupID an unique Base64 ID
+ *
+ * @param setupID     an unique Base64 ID
  * @param _keepStatus toggles if the Switch should save State over program restart (failure)
  * @param _systemCode The SystemCode used for the Switch
- * @param _unitCode The UnitCode used for the Switch
+ * @param _unitCode   The UnitCode used for the Switch
  */
 case class mhzSwitch(setupID: String, _keepStatus: Boolean, private var _systemCode: String, private var _unitCode: String) extends Switch(setupID, _keepStatus) {
   if (_systemCode.length != 5) throw new IllegalArgumentException(s"System code ${_systemCode} should be 5 Char long")
@@ -21,6 +21,7 @@ case class mhzSwitch(setupID: String, _keepStatus: Boolean, private var _systemC
 
   /**
    * Changes the SystemCode
+   *
    * @param newSystemCode The new System Code
    */
   def systemCode_(newSystemCode: String): Unit = {
@@ -31,6 +32,7 @@ case class mhzSwitch(setupID: String, _keepStatus: Boolean, private var _systemC
 
   /**
    * Changes the Unit Code
+   *
    * @param newUnitCode The new Unit Code
    */
   def unitCode_(newUnitCode: String): Unit = {
@@ -51,14 +53,23 @@ case class mhzSwitch(setupID: String, _keepStatus: Boolean, private var _systemC
 
   /**
    * Generates a XML Node containing all Information to Regenerate the object
+   *
    * @return The Node Object
    */
   override def toXml: Node = <switch>
     <type>433MHz</type>
-    <id>{id()}</id>
-    <keepStatus>{_keepStatus}</keepStatus>
-    <systemCode>{systemCode}</systemCode>
-    <unitCode>{unitCode}</unitCode>
+    <id>
+      {id()}
+    </id>
+    <keepStatus>
+      {_keepStatus}
+    </keepStatus>
+    <systemCode>
+      {systemCode}
+    </systemCode>
+    <unitCode>
+      {unitCode}
+    </unitCode>
   </switch>
 
   /**
