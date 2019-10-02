9b085aba85becef6894dfac377360dba8c7aaafc
RaHoni <honi2002suess@gmail.com>
2019-10-02 21:58:37 +0200

    Changed the send function so that it no longer blocks the CPU.

--- src/main/scala/PrivateHome/Devices/MHz/MHz_Connect.scala
+++ src/main/scala/PrivateHome/Devices/MHz/MHz_Connect.scala
@@ -6,77 +6,92 @@ import scala.collection.mutable
 
 class MHz_Connect(Repeat: Int = 10, pulseLength: Long = 350) {
 
+
+  /**
+   * Waits for a given time in microseconds busy on one Core
+   * @param micros Time the Process should wait
+   */
   def busyWaitMicro(micros: Long): Unit = {
     val waitUntil: Long = System.nanoTime() + (micros * 1_000)
     while (waitUntil > System.nanoTime()) {}
   }
 
+  val queue = new mutable.Queue[mhzCommand]
+
+  private var sending = false
+
   val gpio: GpioController = GpioFactory.getInstance()
 
   val output: GpioPinDigitalOutput = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, "433_Out", PinState.LOW)
 
-  
 
-  def send(): Unit = {
+  def send(pCommand: mhzCommand): Unit = {
+    queue.enqueue(pCommand)
+    if (!sending) sendCommand()
 
-    while (true) {
-      if (queue.queue.nonEmpty) {
 
-        val queuedCommand = queue.queue.dequeue()
+  }
 
-        if (!((queuedCommand.systemCode.length == 5) && (queuedCommand.unitCode.length == 5))) {
-          throw new IllegalArgumentException
-        }
+  private def sendCommand(): Unit = {
 
-        var code: Long = 0
-        var length: Int = 0
+    sending = true
 
-        for (c: Char <- queuedCommand.systemCode) {
-          code <<= 2
-          if (c == '0') code |= 0x01
-          length += 2
-        }
+    while (queue.nonEmpty) {
 
-        for (c: Char <- queuedCommand.unitCode) {
-          code <<= 2
-          if (c == '0') code |= 0x01
-          length += 2
-        }
+      val queuedCommand = queue.dequeue()
 
+      if (!((queuedCommand.systemCode.length == 5) && (queuedCommand.unitCode.length == 5))) {
+        throw new IllegalArgumentException
+      }
+
+      var code: Long = 0
+      var length: Int = 0
+
+      for (c: Char <- queuedCommand.systemCode) {
         code <<= 2
-        if (!queuedCommand.command) code |= 0x01
+        if (c == '0') code |= 0x01
         length += 2
+      }
+
+      for (c: Char <- queuedCommand.unitCode) {
         code <<= 2
-        if (queuedCommand.command) code |= 0x01
+        if (c == '0') code |= 0x01
         length += 2
+      }
+
+      code <<= 2
+      if (!queuedCommand.command) code |= 0x01
+      length += 2
+      code <<= 2
+      if (queuedCommand.command) code |= 0x01
+      length += 2
 
 
-        for (count <- 0 until Repeat) {
-          for (i <- length - 1 to 0 by -1) {
-            if (((code >> i) & 0x1) == 1) {
-              output.high()
-              busyWaitMicro(3 * pulseLength)
-              output.low()
-              busyWaitMicro(pulseLength)
+      for (count <- 0 until Repeat) {
+        for (i <- length - 1 to 0 by -1) {
+          if (((code >> i) & 0x1) == 1) {
+            output.high()
+            busyWaitMicro(3 * pulseLength)
+            output.low()
+            busyWaitMicro(pulseLength)
 
-            } else {
-              output.high()
-              busyWaitMicro(pulseLength)
-              output.low()
-              busyWaitMicro(3 * pulseLength)
-            }
+          } else {
+            output.high()
+            busyWaitMicro(pulseLength)
+            output.low()
+            busyWaitMicro(3 * pulseLength)
           }
-
-          output.high()
-          busyWaitMicro(pulseLength)
-          output.low()
-          busyWaitMicro(31 * pulseLength)
-
-          println()
         }
 
+        output.high()
+        busyWaitMicro(pulseLength)
+        output.low()
+        busyWaitMicro(31 * pulseLength)
+
+        println()
       }
     }
+    sending = false
   }
 
 
@@ -85,9 +100,5 @@ class MHz_Connect(Repeat: Int = 10, pulseLength: Long = 350) {
 object mhzCommand {
   def apply(systemCode: String, unitCode: String, command: Boolean): mhzCommand = new mhzCommand(systemCode, unitCode, command)
 }
-case class mhzCommand(systemCode:String, unitCode:String, command:Boolean)
 
-object queue {
-
-  val queue = new mutable.Queue[mhzCommand]
-}
+case class mhzCommand(systemCode: String, unitCode: String, command: Boolean)
