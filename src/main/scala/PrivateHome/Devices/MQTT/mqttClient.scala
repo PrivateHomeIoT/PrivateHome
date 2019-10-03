b4512fbf75a76a1b1ba6c06bd1aeeb6e1391d84c
Maximilian Inckmann <maincky@gmail.com>
2019-10-03 21:13:49 +0200

    Now you can use lastMsg and lastID to find out, which device sent or got a command. Adding some comments...

--- src/main/scala/PrivateHome/Devices/MQTT/mqttClient.scala
+++ src/main/scala/PrivateHome/Devices/MQTT/mqttClient.scala
@@ -6,35 +6,50 @@ import org.eclipse.paho.client.mqttv3.{MqttClient, MqttMessage, _}
 object mqttClient {
 
   private val brokerUrl = "tcp://localhost:429"
-  //private val stat = "Home/switch/stat/#"
-  //private val cmnd = "Home/switch/cmnd/#"
   private val home = "Home/#"
   private val persistence = new MemoryPersistence
   private val client = new MqttClient(brokerUrl, MqttClient.generateClientId, persistence)
-  var lastMessage: String = ""
-  var lastTopic: String = ""
+
+  var lastMsg: String = "" //here you can get the last recieved message
+  var lastID: String = "" //her you can get the last recieved id
 
   client.connect
-
-  //client.subscribe(stat)
-  //client.subscribe(cmnd)
   client.subscribe(home)
 
   val callback = new MqttCallback {
+
+    /**
+     * This method acts after recieving an MQTT-Message
+     *
+     * @param topic   is the topic in which the message was recieved. Mostly it contains the id, which you can see better in lastID.
+     * @param message is the message which was recieved. You can also get it from lastMsg.
+     */
     override def messageArrived(topic: String, message: MqttMessage): Unit = {
       println("Receiving Data, Topic : %s, Message : %s".format(topic, message))
-      lastMessage = message.toString
-      lastTopic = topic
+      lastMsg = message.toString
+      lastID = ""
+      lastID = topic.substring(topic.length - 5)
     }
 
+    /**
+     * This is the method, which acts if the connection is lost
+     *
+     * @param cause is the error, why the connection is lost.
+     */
     override def connectionLost(cause: Throwable): Unit = println(cause)
 
+    /**
+     * I think, it's the method, which acts after sending a MQTT-Message
+     *
+     * @param token I don't know what it is ... It's for internal usage.
+     */
     override def deliveryComplete(token: IMqttDeliveryToken): Unit = {
     }
   }
   client.setCallback(callback)
 
   /**
+   * This method publishes a message in a topic.
    *
    * @param topic   Is the topic in which you want to publish your message.
    * @param message Is the message you want to publish.
