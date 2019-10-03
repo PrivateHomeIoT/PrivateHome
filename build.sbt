cb7a62564f16a9b3bb89caa90e7693e26f41ec53
Maximilian Inckmann <maincky@gmail.com>
2019-10-03 20:36:07 +0200

    Added some comments and some things in build.sbt .

--- build.sbt
+++ build.sbt
@@ -10,3 +10,4 @@ libraryDependencies ++= Seq (
 libraryDependencies += "com.pi4j" % "pi4j-parent" % "1.2"
 libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.2.0"
 libraryDependencies += "org.eclipse.paho" % "mqtt-client" % "0.4.0"
+resolvers += "MQTT Repository" at "https://repo.eclipse.org/content/repositories/paho-releases/"
--- src/main/scala/PrivateHome/Devices/MQTT/mqttSwitch.scala
+++ src/main/scala/PrivateHome/Devices/MQTT/mqttSwitch.scala
@@ -6,10 +6,20 @@ import scala.xml.Node
 
 case class mqttSwitch(ID: String, keepStatus: Boolean) extends Switch(ID, keepStatus) {
 
+  /**
+   * This method turns on the MQTT-Device.
+   */
   def on(): Unit = mqttClient.publish("Home/switch/cmnd/" + id, "ON")
 
+  /**
+   * This method turns off the MQTT-Device.
+   */
   def off(): Unit = mqttClient.publish("Home/switch/cmnd/" + id, "OFF")
 
+  /**
+   *  This method generates from the attributes of this class an XML which you can save with editXML.scala
+   * @return It returns a Node which you can use in editXML.scala
+   */
   override def toXml: Node = <switch id={ID}>
     <type>MQTT</type>
     <id>{ID}</id>
