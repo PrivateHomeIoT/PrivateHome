2ad3750da665d9381ebd13bed482159495afe0ca
Maximilian Inckmann <maincky@gmail.com>
2019-10-03 22:05:40 +0200

    Now, mqttSwitch is a class and not a case class anymore

--- src/main/scala/PrivateHome/Devices/MQTT/mqttSwitch.scala
+++ src/main/scala/PrivateHome/Devices/MQTT/mqttSwitch.scala
@@ -4,7 +4,7 @@ import PrivateHome.Devices.Switch
 
 import scala.xml.Node
 
-case class mqttSwitch(ID: String, keepStatus: Boolean) extends Switch(ID, keepStatus) {
+class mqttSwitch(ID: String, keepStatus: Boolean) extends Switch(ID, keepStatus) {
 
   /**
    * This method turns on the MQTT-Device.
