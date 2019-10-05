97c21d8924f6e976a4f66e17d0402876dfd9bffa
Maximilian Inckmann <maincky@gmail.com>
2019-10-05 22:58:29 +0200

    Made val to var to be able to add elements when the script is running...

--- src/main/scala/PrivateHome/data.scala
+++ src/main/scala/PrivateHome/data.scala
@@ -6,21 +6,21 @@ import scala.xml._
 
 object data {
 
-  val IDs: List[String] = {
+  var IDs: List[String] = {
     val xml = XML.load("src/main/scala/PrivateHome/id.xml")
     val prep: NodeSeq  = xml\\"id"
     val result = for (i <- (0 until prep.length)) yield prep(i).text
     result.toList
   }
 
-  val devices: Map[String, Switch] = {
+  var devices: Map[String, Switch] = {
     val xml: Elem = XML.load("src/main/scala/PrivateHome/devices.xml")
     val prep: NodeSeq = xml\"switch"
     val data: Seq[(String, Switch)] = for(i <- prep) yield (i\"@id").text -> Switch(i)
     data.toMap
   }
 
-  val mhzID: Map[String, String] = {
+  var mhzID: Map[String, String] = {
     val xml: Elem = XML.load("src/main/scala/PrivateHome/devices.xml")
     val prep: NodeSeq = xml\"switch"
     val data: Seq[(String, String)] = for(i<-prep) yield ((i\"systemCode").text + (i\"unitCode").text) -> (i\"@id").text
