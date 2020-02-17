067b4d4dcc6082c880d5b1aa56e190403c3ac98b
RaHoni <honi2002suess@gmail.com>
2020-02-17 16:12:12 +0100

    First working product

--- src/test/scala/PrivateHome/dataSQLTest.scala
+++ src/test/scala/PrivateHome/dataSQLTest.scala
@@ -1,12 +1,26 @@
 package PrivateHome
 
-import org.scalatest.FunSuite
+import PrivateHome.Devices.MHz.mhzSwitch
+import org.scalatest.{Outcome, fixture}
+import org.scalatest.fixture.FunSuite
+import scalikejdbc._
 
-class dataSQLTest extends FunSuite {
+class dataSQLTest extends fixture.FunSuite  {
 
-  test("getDevices") {
+  type FixtureParam = AutoSession
+
+  override def withFixture(test: OneArgTest): Outcome = {
+    Class.forName("org.h2.Driver")
+    ConnectionPool.singleton("jdbc:h2:./daten/hello", "user", "pass")
+    val session: AutoSession.type = AutoSession
+    test(session)
+  }
+
+  test("getDevices") { psession =>
+    implicit val session: AutoSession = psession
     dataSQL.create()
-    println (dataSQL.getDevices)
+    dataSQL.addDevice(mhzSwitch("tesef", _keepStatus = true, "10000", "10101"))
+    println(dataSQL.getDevices)
   }
 
 }
