eee66206e8388b91e91d0ef1d20bde14f2fe4af1
RaHoni <honi2002suess@gmail.com>
2020-02-17 16:13:46 +0100

    Regenerating dataSQLTest

--- src/test/scala/PrivateHome/dataSQLTest.scala
+++ src/test/scala/PrivateHome/dataSQLTest.scala
@@ -1,26 +1,19 @@
 package PrivateHome
 
-import PrivateHome.Devices.MHz.mhzSwitch
-import org.scalatest.{Outcome, fixture}
-import org.scalatest.fixture.FunSuite
-import scalikejdbc._
+import org.scalatest.FunSuite
 
-class dataSQLTest extends fixture.FunSuite  {
+class dataSQLTest extends FunSuite {
 
-  type FixtureParam = AutoSession
+  test("testGetDevices") {
 
-  override def withFixture(test: OneArgTest): Outcome = {
-    Class.forName("org.h2.Driver")
-    ConnectionPool.singleton("jdbc:h2:./daten/hello", "user", "pass")
-    val session: AutoSession.type = AutoSession
-    test(session)
   }
 
-  test("getDevices") { psession =>
-    implicit val session: AutoSession = psession
-    dataSQL.create()
-    dataSQL.addDevice(mhzSwitch("tesef", _keepStatus = true, "10000", "10101"))
-    println(dataSQL.getDevices)
+  test("testCreate") {
+
+  }
+
+  test("testAddDevice") {
+
   }
 
 }
