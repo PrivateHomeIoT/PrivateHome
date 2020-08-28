e8b7cb298972a2aa1615f668338952ed5059f2dc
RaHoni <honisuess@gmail.com>
2020-08-28 23:07:24 +0200

    Added an check for The port numbers so that they are not aut of range

--- src/main/scala/PrivateHome/settings.scala
+++ src/main/scala/PrivateHome/settings.scala
@@ -6,6 +6,7 @@ case object settings {
   var http:http = new http(2000) //ToDo: change to 80 in produktion
   var database = new database(userName = "user", password = "pass")
 }
-case class http(port:Int)
+
+case class http(port:Int) {if (port<0||port>0xffff) throw new IllegalArgumentException("Argument Port aut of bound must be between 0x0 and 0xffff=65536")}
 case class database(userName:String,password: String)
 
