0ab92b8f3615c926d0379df5d75d73274be392ed
RaHoni <honisuess@gmail.com>
2020-08-28 22:17:59 +0200

    Added an extra Config for an normal Http Server and therfor renamed the old http var to websocket

--- src/main/scala/PrivateHome/settings.scala
+++ src/main/scala/PrivateHome/settings.scala
@@ -2,7 +2,8 @@ package PrivateHome
 
 //ToDo: make settings for an config file
 case object settings {
-  var http: http = new http(port = 2888)
+  var websocket: http = new http(port = 2888)
+  var http:http = new http(2000) //ToDo: change to 80 in produktion
   var database = new database(userName = "user", password = "pass")
 }
 
