eeb3b6bee8f0a685db5611e47cf1d0f7c9bf3e1d
RaHoni <honisuess@gmail.com>
2020-08-28 22:15:14 +0200

    In dataTest.scala was the name of the Boolean parameter not jet corrected to prevent this Error in the future the boolean Parameters aren't named anymore. Also Have I forgotten to add Names to the Switches.

--- src/test/scala/PrivateHome/dataTest.scala
+++ src/test/scala/PrivateHome/dataTest.scala
@@ -9,56 +9,57 @@ import org.scalatest.FunSuite
 class dataTest extends FunSuite {
 
 
-//  test("testCreate") {
-//    data.create()
-//  }
-//
-//  test("At Create empty") {
-//    val expected: Map[String, Switch] = Map()
-//    data.create()
-//    data.fillDevices()
-//    assertResult(expected)(data.devices)
-//  }
-//
-//  test("Test: Add one Device") {
-//    data.create()
-//    data.addDevice(mqttSwitch("abcde", keepStatus = false))
-//    var expected: Map[String, Switch] = Map()
-//    expected = expected + (("abcde", mqttSwitch("abcde", keepStatus = false)))
-//    data.fillDevices()
-//    assertResult(expected)(data.devices)
-//
-//  }
-//
-//  test("Test: Add more Devices") {
-//    data.create()
-//    var expected: Map[String, Switch] = Map(("abcde", mqttSwitch("abcde", keepStatus = true)),("hkIKH",mhzSwitch("hkIKH",_keepStatus = false,"10101","01010")))
-//    val expected2: Map[String, Switch] = Map(("kijhe",mhzSwitch("kijhe",_keepStatus = false,"11111","00000")),("testd",mqttSwitch("testd",keepStatus = false)))
-//    expected.foreach(d => data.addDevice(d._2))
-//
-//    data.fillDevices()
-//    assertResult(expected)(data.devices)
-//    expected2.foreach(d => data.addDevice(d._2))
-//    expected = expected.concat(expected2)
-//    data.fillDevices()
-//    assertResult(expected)(data.devices)
-//
-//  }
-//
-//  test("Save State") {
-//    data.create()
-//    data.addDevice(mqttSwitch("abcde", keepStatus = false))
-//    var expected: Map[String, Switch] = Map()
-//    expected = expected + (("abcde", mqttSwitch("abcde", keepStatus = false)))
-//    data.fillDevices()
-//  }
-//
-//  test("Test: getDevice") {
-//    data.create()
-//    var switch = mqttSwitch("abcde", keepStatus = false)
-//    data.addDevice(switch)
-//    assertResult(data.getDevice("abcde"))(switch)
-//  }
+  test("testCreate") {
+    data.create()
+  }
+
+  test("At Create empty") {
+    val expected: Map[String, Switch] = Map()
+    data.create()
+    data.fillDevices()
+    assertResult(expected)(data.devices)
+  }
+
+  test("Test: Add one Device") {
+    data.create()
+    data.addDevice(mqttSwitch("abcde", false,"Device one"))
+    var expected: Map[String, Switch] = Map()
+    expected = expected + (("abcde", mqttSwitch("abcde",  false,"Device one")))
+    data.fillDevices()
+    assertResult(expected)(data.devices)
+
+  }
+
+  test("Test: Add more Devices") {
+    data.create()
+    var expected: Map[String, Switch] = Map(("abcde", mqttSwitch("abcde", true,"Device two")),("hkIKH",mhzSwitch("hkIKH",_keepStatus = false,"Device three","10101","01010")))
+    val expected2: Map[String, Switch] = Map(("kijhe",mhzSwitch("kijhe",_keepStatus = false,"Device Four","11111","00000")),("testd",mqttSwitch("testd", false,"Device five")))
+    expected.foreach(d => data.addDevice(d._2))
+
+    data.fillDevices()
+    assertResult(expected)(data.devices)
+    expected2.foreach(d => data.addDevice(d._2))
+    expected = expected.concat(expected2)
+    data.fillDevices()
+    assertResult(expected)(data.devices)
+
+  }
+
+  test("Save State") {
+    data.create()
+    data.addDevice(mqttSwitch("abcde", false,"Device"))
+    var expected: Map[String, Switch] = Map()
+    expected = expected + (("abcde", mqttSwitch("abcde", false,"Device")))
+    data.fillDevices()
+  }
+
+  test("Test: getDevice") {
+    data.create()
+    val switch = mqttSwitch("abcde", false,"Test")
+    data.addDevice(switch)
+    assertResult(data.getDevice("abcde"))(switch)
+  }
+
 
 
 
