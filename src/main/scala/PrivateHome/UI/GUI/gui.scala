eee1595fd4ec2dc51d97833f6b9af932ca2caeec
RaHoni <honisuess@gmail.com>
2020-08-28 23:28:11 +0200

    Removed unnecessary Imports

--- src/main/scala/PrivateHome/UI/GUI/gui.scala
+++ src/main/scala/PrivateHome/UI/GUI/gui.scala
@@ -1,16 +1,12 @@
 package PrivateHome.UI.GUI
 
 import PrivateHome.UI.Websocket.websocket
-import PrivateHome.{data, settings}
+import PrivateHome.settings
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives.{extractRequest, handleWebSocketMessages, path}
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.settings.{ServerSettings, WebSocketSettings}
-
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
 
 object gui {
   implicit val actorSystem: ActorSystem = ActorSystem("system")
