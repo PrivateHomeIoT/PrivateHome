abc1ba2701c818de451f2a179d566c6fa449f250
RaHoni <honisuess@gmail.com>
2020-08-29 14:07:31 +0200

    Added an HTTP route to gui.scala and to give the possibility to change the directory  I have added an Setting for the path to the settings (also websocket.

--- src/main/scala/PrivateHome/UI/GUI/gui.scala
+++ src/main/scala/PrivateHome/UI/GUI/gui.scala
@@ -4,19 +4,25 @@ import PrivateHome.UI.Websocket.websocket
 import PrivateHome.settings
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Directives.{extractRequest, handleWebSocketMessages, path}
+import akka.http.scaladsl.server.Directives.{extractRequest, getFromBrowseableDirectory, handleWebSocketMessages, path}
 import akka.http.scaladsl.server.Route
 import scala.concurrent.duration._
 
 object gui {
   implicit val actorSystem: ActorSystem = ActorSystem("system")
-  val websocketRoute: Route = path("ws") {
+  val websocketRoute: Route = path(settings.websocket.path) {
     extractRequest { request =>
       handleWebSocketMessages(websocket.listen(request.getHeader("Sec-WebSocket-Key").get().value()))
     }
   }
   
   Http().newServerAt("0.0.0.0",settings.websocket.port).adaptSettings(_.mapWebsocketSettings(_.withPeriodicKeepAliveMaxIdle(1.second))).bind(websocketRoute)
+
+
+  val httpRoute: Route =  {
+    getFromBrowseableDirectory(settings.http.path)
+  }
+  Http().newServerAt("0.0.0.0",settings.http.port).bind(httpRoute)
 
 
 }
