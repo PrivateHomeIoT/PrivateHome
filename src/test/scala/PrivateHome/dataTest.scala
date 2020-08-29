7c6f6cdbe9dff695e1a90a4d361578aa4baea015
RaHoni <honisuess@gmail.com>
2020-08-29 14:07:59 +0200

    Upgraded scalatest

--- src/test/scala/PrivateHome/dataTest.scala
+++ src/test/scala/PrivateHome/dataTest.scala
@@ -3,10 +3,10 @@ package PrivateHome
 import PrivateHome.Devices.MHz.mhzSwitch
 import PrivateHome.Devices.MQTT.mqttSwitch
 import PrivateHome.Devices.Switch
-import org.scalatest.FunSuite
+import org.scalatest.funsuite.AnyFunSuite
 
 
-class dataTest extends FunSuite {
+class dataTest extends AnyFunSuite {
 
 
   test("testCreate") {
@@ -22,9 +22,9 @@ class dataTest extends FunSuite {
 
   test("Test: Add one Device") {
     data.create()
-    data.addDevice(mqttSwitch("abcde", false,"Device one"))
+    data.addDevice(mqttSwitch("abcde", false, "Device one"))
     var expected: Map[String, Switch] = Map()
-    expected = expected + (("abcde", mqttSwitch("abcde",  false,"Device one")))
+    expected = expected + (("abcde", mqttSwitch("abcde", false, "Device one")))
     data.fillDevices()
     assertResult(expected)(data.devices)
 
@@ -32,8 +32,8 @@ class dataTest extends FunSuite {
 
   test("Test: Add more Devices") {
     data.create()
-    var expected: Map[String, Switch] = Map(("abcde", mqttSwitch("abcde", true,"Device two")),("hkIKH",mhzSwitch("hkIKH",_keepStatus = false,"Device three","10101","01010")))
-    val expected2: Map[String, Switch] = Map(("kijhe",mhzSwitch("kijhe",_keepStatus = false,"Device Four","11111","00000")),("testd",mqttSwitch("testd", false,"Device five")))
+    var expected: Map[String, Switch] = Map(("abcde", mqttSwitch("abcde", true, "Device two")), ("hkIKH", mhzSwitch("hkIKH", _keepStatus = false, "Device three", "10101", "01010")))
+    val expected2: Map[String, Switch] = Map(("kijhe", mhzSwitch("kijhe", _keepStatus = false, "Device Four", "11111", "00000")), ("testd", mqttSwitch("testd", false, "Device five")))
     expected.foreach(d => data.addDevice(d._2))
 
     data.fillDevices()
@@ -47,19 +47,18 @@ class dataTest extends FunSuite {
 
   test("Save State") {
     data.create()
-    data.addDevice(mqttSwitch("abcde", false,"Device"))
+    data.addDevice(mqttSwitch("abcde", false, "Device"))
     var expected: Map[String, Switch] = Map()
-    expected = expected + (("abcde", mqttSwitch("abcde", false,"Device")))
+    expected = expected + (("abcde", mqttSwitch("abcde", false, "Device")))
     data.fillDevices()
   }
 
   test("Test: getDevice") {
     data.create()
-    val switch = mqttSwitch("abcde", false,"Test")
+    val switch = mqttSwitch("abcde", false, "Test")
     data.addDevice(switch)
     assertResult(data.getDevice("abcde"))(switch)
   }
-
 
 
 }
