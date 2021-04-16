4968d0208498e58a5f04d701e980b5191650ccf8
RaHoni <honi2002suess@gmail.com>
2021-04-16 20:24:26 +0200

    uiControl API: uiAPI extension for Controller
    Extended uiControl by
    - commandAddController
    - commandGetController
    
    where commandGetController returns a list of the controller ids and their names
    
    For websocket they get simply cast to json ```Args:{"masterId":"aaaaa","name":"Name"}``` and for the console the format is ```<id>:<name>,...```
    
    Additional api in stringCommandHandler specific for the repl getControllerKey: which returns the keyArray as `,` separated hex numbers

--- src/main/scala/PrivateHome/UI/stringCommandHandler.scala
+++ src/main/scala/PrivateHome/UI/stringCommandHandler.scala
@@ -1,8 +1,28 @@
+/*
+ * Privatehome
+ *     Copyright (C) 2021  RaHoni honisuess@gmail.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package PrivateHome.UI
 
 import PrivateHome.data
+import PrivateHome.data.chars
 import org.slf4j.LoggerFactory
 
+import java.lang.Integer.parseInt
 import java.math.BigInteger
 import java.security.SecureRandom
 
@@ -21,36 +41,42 @@ object stringCommandHandler {
         case "commandRecreateDatabase" => commandRecreateDatabase()
         case "commandSafeCreateDatabase" => commandSafeCreateDatabase()
         case "getRandomId" =>
-          var id: String = null
+          logger.debug("Running getRandomId")
+          var id: String = ""
           val random = new SecureRandom()
           var run = true
           while (run) {
-
-            id = new BigInteger(5 * 5, random).toString(32) //  This generates a random String with length 5
+            id = ""
+            new BigInteger(5 * 6, random).toString(2).grouped(6).foreach(t => id += chars(parseInt(t,2))) //  This generates a random String with length 5 and the chars -_a-Z0-9
             try {
               data.idTest(id, create = true)
-              logger.debug("Recomended id: \"{}\"",id)
+              logger.debug("Recomended id: \"{}\"", id)
               run = false
             }
             catch {
-              case _: IllegalArgumentException =>
+              case t: IllegalArgumentException => logger.debug("Error while generating randomID",t)
+              case t: Exception => logger.warn("Error while generating randomID",t)
             }
           }
           id
         case "commandOn" => commandOn(args(0), args(1))
         case "commandOff" => commandOff(args(0))
-        case "commandAddDevice" => commandAddDevice(args(0), args(1), args(2), args(3), args(4), args(5), args(6).toBoolean)
+        case "commandAddDevice" => commandAddDevice(args(0), args(1), args(2), args(3), args(4), args(5), args(6).toBoolean, args(7).toInt, args(8))
+        case "commandAddController" => commandAddController(args(0))
+        case "commandGetController" => commandGetController()
+        case "getControllerKey" => data.getControllerMasterId(args(0)).keyArray.map("%02X" format _).mkString(";")
 
         case _ => logger.warn("Unknown Command")
           new Command
       }
       uiCommand match {
-        case c:Command => uiControl.receiveCommand(c)
-        case s:String => s
+        case c: Command => uiControl.receiveCommand(c)
+        case s: String => s
+        case _ => ""
       }
 
     } catch {
-      case e: Throwable => logger.error("Unknown Error while interpreting Console command",e)
+      case e: Throwable => logger.error("Unknown Error while interpreting Console command", e)
         e + e.getStackTrace.mkString("\n")
     }
   }
