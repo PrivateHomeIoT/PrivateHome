34164ab450111f330c9e1c25310e83cfdede5d74
Maximilian Inckmann <maximilianinck@icloud.com>
2021-04-16 16:55:54 +0200

    Finished setup page
    Implemented method to react on "randomID" message of the server
    
    Duplicated settings.js with minor changes to setup.js
    
    setup.js sends "createDevice" with the same args as "updateDevice" on submit
    
    setup.js sends "getRandomID" without args on wsOpen to get a random ID

---
+++ src/main/resources/Website/JS/setup.js
@@ -0,0 +1,90 @@
+var randomID;
+
+ws.onmessage = function(event) {
+  var msg = JSON.parse(event.data);
+  switch (msg.Command) {
+    case "getDevices": {
+      break;
+    }
+    case "getDevice":{
+      break;
+    }
+    case "newDevice": {
+      break;
+    }
+    case "deviceChange":{
+      break;
+    }
+    case "randomID":{
+      setID(msg.answer);
+    }
+    default: { console.log(msg.error.toString() + ": " + msg.exception);}
+  }
+}
+
+function setID(args){
+  let answer = arguments;
+  randomID = answer.id;
+}
+
+ws.onopen = function (event) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  ws.send(JSON.stringify({Command:"getRandomID",Args:{}}));
+}
+
+function sendNewData() {
+  form = document.getElementById("form")
+  var arguments = new Object();
+  arguments.randomID = randomID;
+  arguments.newId = form.idtext.value;
+  arguments.keepState = form.keepState.checked;
+  arguments.name = form.name.value;
+  if (form.controlType.checked) {
+    arguments.controlType = "slider";
+    arguments.switchType = "mqtt";
+  } else {
+    arguments.controlType = "button";
+    arguments.switchType = form.switchType.value;
+    arguments.systemCode = form.systemCode.value;
+    arguments.unitCode = form.unitCode.value;
+  }
+  console.log(JSON.stringify(arguments));
+  ws.send(JSON.stringify({Command:"createDevice",Args:arguments}));
+}
+
+
+function registerHandler() {
+  form = document.getElementById("form")
+  form.addEventListener( "submit", function ( event ) {
+    event.preventDefault();
+    sendNewData();
+  });
+  form.controlType.addEventListener("change", function (event) {
+    disable = form.controlType.checked
+    form.systemCode.disabled = disable;
+    form.unitCode.disabled = disable;
+    form.switchType.disabled = disable;
+    if (disable) {
+    form.switchType.value = "mqtt";
+    form.unitCode.value = "";
+    form.systemCode.value = "";
+    }
+  });
+  form.switchType.addEventListener("change", function (event) {
+    disable = form.switchType.value != "433Mhz";
+    form.systemCode.disabled = disable;
+    form.unitCode.disabled = disable;
+    if (disable) {
+      form.unitCode.value = "";
+      form.systemCode.value = "";
+    }
+  });
+}
+
+window.addEventListener('load', function () {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  document.getElementById("idtext").value = urlParams.get("id");
+  registerHandler();
+});
