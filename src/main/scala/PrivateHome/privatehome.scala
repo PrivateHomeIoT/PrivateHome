c2381959edce65616d790a2bb340cc4d696985a6
RaHoni <honi2002suess@gmail.com>
2021-04-16 20:02:57 +0200

    Logback: Marker support for Switch command logging
    So that you can toggle if displaying of every Command on level INFO if -DlogCommands=ALLOW is set as jvm option.

--- src/main/scala/PrivateHome/privatehome.scala
+++ src/main/scala/PrivateHome/privatehome.scala
@@ -1,3 +1,21 @@
+/*
+ * Privatehome
+ *     Copyright (C) 2021  RaHoni honisuess@gmail.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package PrivateHome
 
 import PrivateHome.Devices.MHz.sendMhz
@@ -10,6 +28,15 @@ import sun.misc.{Signal, SignalHandler}
 
 object privatehome {
   val portable: Boolean = !getClass.getProtectionDomain.getCodeSource.getLocation.toURI.getPath.startsWith("/usr/share/privatehome/")
+
+  val logCommands = System.getProperty("logCommands","notSpecified")
+
+  if (logCommands.equals("ALLOW") || logCommands.equals("NATURAL") || logCommands.equals("DENY")) {
+
+    System.setProperty("MARKER.COMMAND",logCommands)
+  } else if (!logCommands.equals("notSpecified")) {
+    System.setProperty("MARKER.COMMAND","ALLOW")
+  }
 
   if (portable)
     System.setProperty("log.logpath","./logs/")
