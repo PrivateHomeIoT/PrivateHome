013e4922c15820eea2d1df8e51811c84849e4c86
RaHoni <honi2002suess@gmail.com>
2021-04-16 20:02:57 +0200

    Logback: Marker support for Switch command logging
    So that you can toggle if displaying of every Command on level INFO if -DlogCommands=ALLOW is set as jvm option.

--- src/main/scala/PrivateHome/Devices/MHz/mhzSwitch.scala
+++ src/main/scala/PrivateHome/Devices/MHz/mhzSwitch.scala
@@ -1,6 +1,25 @@
+/*
+ * Privatehome
+ *     Copyright (C) 2021  RaHoni honisuess@gmail.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package PrivateHome.Devices.MHz
 
 import PrivateHome.Devices.Switch
+import org.slf4j.{LoggerFactory, MarkerFactory}
 
 import scala.xml.Node
 
@@ -19,6 +38,8 @@ case class mhzSwitch(setupID: String, _keepStatus: Boolean, _name: String, priva
   if (!_systemCode.matches("[01]{5}")) throw new IllegalArgumentException(s"System code ${_systemCode} should only contain 0/1")
   if (!_unitCode.matches("[01]{5}")) throw new IllegalArgumentException(s"Unit code ${_unitCode} should only contain 0/1")
 
+  private val logger = LoggerFactory.getLogger(this.getClass)
+  private val commandMarker = MarkerFactory.getMarker("COMMAND")
   /**
    * Changes the SystemCode
    *
@@ -44,12 +65,18 @@ case class mhzSwitch(setupID: String, _keepStatus: Boolean, _name: String, priva
   /**
    * Turns the Switch on
    */
-  override def on(percent:  Float): Unit = sendMhz(mhzCommand(_systemCode, _unitCode, command = true))
+  override def on(percent:  Float): Unit = {
+    logger.info(commandMarker, "Turning on Switch {} to {}%", name, 100)
+    sendMhz(mhzCommand(_systemCode, _unitCode, command = true))
+  }
 
   /**
    * Turns the Switch off
    */
-  override def off(): Unit = sendMhz(mhzCommand(_systemCode, _unitCode, command = false))
+  override def off(): Unit = {
+    logger.info(commandMarker, "Turning off Switch {}", name)
+    sendMhz(mhzCommand(_systemCode, _unitCode, command = false))
+  }
 
   /**
    * Generates a XML Node containing all Information to Regenerate the object
