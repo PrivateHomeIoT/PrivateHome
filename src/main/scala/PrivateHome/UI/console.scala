82e1fb2619dc3d78aa2c0cc9011df5eeb8fad833
RaHoni <honisuess@gmail.com>
2022-08-23 11:15:05 +0200

    Moved REPLCommand to REPLCommand.scala
    Refactored REPLCommand to no longer being in the file console.scala

--- src/main/scala/PrivateHome/UI/console.scala
+++ src/main/scala/PrivateHome/UI/console.scala
@@ -434,10 +434,4 @@ object console {
 }
 
 
-/**
- * A REPL Command holds a method that can be executed and a description.
- *
- * @param methodToCall the method to call, when the command is executed
- * @param description  the description to show when the user asks for help
- */
-case class REPLCommand(methodToCall: Unit => Unit, description: String)
+
